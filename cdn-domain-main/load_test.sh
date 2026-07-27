a8cef21d3381ccfcbd69d5c9a79bf0f5bd05a749
cdn-domain <zawmoewin120298@gmail.com>
2026-07-27 14:59:07 +0700

    Delete old folder

--- cdn-domain-main/load_test.sh
+++
@@ -1,1 +0,0 @@
-wrk -c10 -t2 -d600s -s ./src/load_tests.lua --latency http://localhost:18080
--- cdn-domain-main/src/edge.lua
+++
@@ -1,8 +0,0 @@
-local simulations = require "simulations"
-local edge = {}
-
-edge.simulate_load = function()
-  simulations.for_work_longtail(simulations.profiles.edge)
-end
-
-return edge
